303413a64b6d0348b71cdf34eb50b323656ef5bf
dujodujo <avsic.ziga@gmail.com>
2013-11-05 12:14:43 +0100

    primus

--- include/noiseutils.h
+++ include/noiseutils.h
@@ -11,7 +11,7 @@
 // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 // FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 // License (COPYING.txt) for more details.
-//
+//fdgdfgdfgdfg
 // You should have received a copy of the GNU Lesser General Public License
 // along with this library; if not, write to the Free Software Foundation,
 // Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
